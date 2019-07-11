f1b7a4c03df2ec4904e46a453bc4d4f775a6bca0
Hemanth <hemanth.aero@gmail.com>
2019-07-11 21:57:53 +0530

    Temporarily turn off CORS

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use('/peerjs', peerserver);
 app.use(function(req, res, next) {
   // TODO Not the most secure way though
   // Refer: https://fosterelli.co/developers-dont-understand-cors
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.setHeader("Access-Control-Request-Method", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  //res.setHeader("Access-Control-Request-Method", "*");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
