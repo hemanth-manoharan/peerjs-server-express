bd72ff135a34b2ba434f57fe6105c2ca9f520dc2
Hemanth <hemanth.aero@gmail.com>
2020-09-20 09:19:10 +0530

    Enabling CORS

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
   // TODO Not the most secure way though
   // Refer: https://fosterelli.co/developers-dont-understand-cors
   //res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "https://webrtc-serverless.herokuapp.com"); 
   //res.setHeader("Access-Control-Request-Method", "*");
   //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
