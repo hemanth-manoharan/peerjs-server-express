2e058b4c81c6d1fc59d44aa1b86e8fb355bda895
Hemanth <hemanth.aero@gmail.com>
2021-05-06 21:16:00 +0530

    Revert access control origin to correct value.

--- index.ts
+++ index.ts
@@ -18,8 +18,8 @@ app.use('/peerjs', peerserver);
 app.use(function(req, res, next) {
   // TODO Not the most secure way though
   // Refer: https://fosterelli.co/developers-dont-understand-cors
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  //res.header("Access-Control-Allow-Origin", "https://webrtc-serverless.herokuapp.com"); 
+  // res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "https://webrtc-serverless.herokuapp.com"); 
   //res.setHeader("Access-Control-Request-Method", "*");
   //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
